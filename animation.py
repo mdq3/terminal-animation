1c1a93a94a8dca39646b756f276cd310cb4c2cfe
mdq3 <bruteforks@gmail.com>
2014-08-12 12:40:29 +0100

    new file:   animation.py 	new file:   terminal_animation.py

---
+++ animation.py
@@ -0,0 +1,220 @@
+frame1 = \
+"                                         .                      \n" + \
+"                                        `-::-``                 \n" + \
+"                                        sdNNmo+                 \n" + \
+"                                       `dNMMMyy                 \n" + \
+"                                        hNMMMyy`                \n" + \
+"                                     .yhNMMmh:-                 \n" + \
+"                                   `:mMMMMmy                    \n" + \
+"                                   -yMMMMMNd.                   \n" + \
+"                                  `oNMMMMMMNo`                  \n" + \
+"                                  .sMMMMMMMNs`                  \n" + \
+"                                  .sMMMMMMMN/                   \n" + \
+"                                  /hMMMMMMMNy:-                 \n" + \
+"                                 `hNMMMMMMMMMmm.                \n" + \
+"                                 +mMMMMMMMMMMNm.`               \n" + \
+"                                 yNMMMMMMMmyoyy.`               \n" + \
+"                                 hNMMMMMMMd+                    \n" + \
+"                                 yNMMMMMMMmo                    \n" + \
+"                                 /dMMMMMMMh/                    \n" + \
+"                                  -sMMMMMMms                    \n" + \
+"                        -+o:-......oNMMMMMMm-                   \n" + \
+"                       `dNMMNmmmmdhmMMMMMMMNo`                  \n" + \
+"                      .oMMMMMMMMMMMMMMMMMMMNy`                  \n" + \
+"                     .+h/:-/oyyhhhddmMMMMMMm:                   \n" + \
+"                     `.`         ```:mMMMMd+                    \n" + \
+"                                   `:NMMMNs.                    \n" + \
+"                                   :hMMMNo.                     \n" + \
+"                                   +NMMMh-                      \n" + \
+"                                  `oNMMMy-                      \n" + \
+"                                   /dNNNNho:                    \n" + \
+"                                      ``..``                    \n"
+
+frame2 = \
+"                                          .                     \n" + \
+"                                        `:++/:.                 \n" + \
+"                                       `+mMMNNo.                \n" + \
+"                                       `oNMMMMs-                \n" + \
+"                                       .oNMMMMo.                \n" + \
+"                                     -odNMMho/.                 \n" + \
+"                                    :yMMMMM+.                   \n" + \
+"                                   .sNMMMMMy-                   \n" + \
+"                                  `dNMMMMMMm/`                  \n" + \
+"                                 `+NMMMMMMMN+``   ``            \n" + \
+"                                 `/NMMMMMMMMdyso+oyy`           \n" + \
+"                                  -NMMMMMMMMMMMMMNys.           \n" + \
+"                                 .hMMMMMMMNo:---:-`             \n" + \
+"                                `:MMMMMMMMh.                    \n" + \
+"                                `/MMMMMMMMh`                    \n" + \
+"                                `:MMMMMMMMMo.                   \n" + \
+"                                `-NMMMMMMMMs-                   \n" + \
+"                                 `oNMMMMMMMo-                   \n" + \
+"                                   yNMMMMMMN+.`                 \n" + \
+"                                   /dMMMMMMMdy-                 \n" + \
+"                            `..--:/ymMMMMMMMMM+.                \n" + \
+"                            +hNNNMMMMMMMMMMMMMs-                \n" + \
+"                            +dMNNmMMMMMNmhyyo/.                 \n" + \
+"                            sdy:/+MMMMMs-`                      \n" + \
+"                            oo: :hMMMmy-                        \n" + \
+"                              ./yMMMd+                          \n" + \
+"                              :dmMMm/`                          \n" + \
+"                             `/mNMMd/`                          \n" + \
+"                              -ooydNds/`                        \n" + \
+"                                  `.-:-`                        \n"
+ 
+frame3 = \
+"                                          `.```                 \n" + \
+"                                        `smMMNm-`               \n" + \
+"                                        -dMMMMM/`               \n" + \
+"                                        -hNMMMM/`               \n" + \
+"                                      `/yNMMNmy.                \n" + \
+"                                  `-+dmNMMMN+/`                 \n" + \
+"                                 -smMMMMMMMMho                  \n" + \
+"                                `hNMMMMMMMMMNh`    ..`          \n" + \
+"                               `-MMMMMMMMMMMMNo-/sdmm+          \n" + \
+"                                `smMMMMMMMMMMMNNNMMms`          \n" + \
+"                                  odMMMMMMMMMMMMNh+-            \n" + \
+"                                  /dMMMMMMMm+++/.`              \n" + \
+"                                 .hMMMMMMMNy                    \n" + \
+"                                 sNMMMMMMMMm/:                  \n" + \
+"                                `mMMMMMMMMMMmh-                 \n" + \
+"                               `:MMMMMMMMMMMMNs`                \n" + \
+"                               `/MMMMMMMMMMMMMNs-               \n" + \
+"                               .sMMMMNd+odNMMMMNs:              \n" + \
+"                              `:NMMMNo/  .+mMMMMMms/            \n" + \
+"                             `/sMMMNd..`:sdNMMMMMNNs.           \n" + \
+"                             -dmMMMNh:/yMMMMMMMNmdy/            \n" + \
+"                            :yMMMMMMNNNMMMMNhyo:-`              \n" + \
+"                           :hMMMMmmNMMNmho:`                    \n" + \
+"                          .hNMMMN-:yNhs-`                       \n" + \
+"                         `oNMMMmo `om+:                         \n" + \
+"                        -:NMMNds`  `--.                         \n" + \
+"                       `ddMMNh..                                \n" + \
+"                       `osmMdo                                  \n" + \
+"                          .shh:.                                \n" + \
+"                            ````                                \n"
+
+frame4 = \
+"                                           .+++/-`              \n" + \
+"                                          .dMMMMd/              \n" + \
+"                                          .mMMMMd+              \n" + \
+"                                         `:mMMMMd+              \n" + \
+"                                     .:ohdNMMNms:`              \n" + \
+"                                  `-smNMMMMMMNm/`  ``           \n" + \
+"                                ./odMMMMMMMMMMMh/ `so/.         \n" + \
+"                               `+NMMMMMMMMMMMMMMmhdMho.         \n" + \
+"                                :ymMMMMMMMMMMMMMMMMNo.`         \n" + \
+"                                `.omMMMMMMMMMNmdmNNo.           \n" + \
+"                                  `/NMMMMMMMMy+.`-/`            \n" + \
+"                                   -mMMMMMMMN+.                 \n" + \
+"                                  :yMMMMMMMMNo-`                \n" + \
+"                                `-sNMMMMMMMMMmh+-               \n" + \
+"                               `/mNMMMMMMMMMMMMms.              \n" + \
+"                               -sMMMMMMMMMMMMMMMNd/`            \n" + \
+"                              `smMMMMNmdmmmmMMMMMMmo-`          \n" + \
+"                              smMMMMN/`    `/ydNMMMMdy:`        \n" + \
+"                            .oMMMMMm:        .-smMMMMN+`        \n" + \
+"                          `:yMMMMMdo        `/smMMMMhs-         \n" + \
+"                        `:sdMMMMds:`       `omMMMMNo-           \n" + \
+"                       .ymMMMMd/.        `-hMMMMmy.             \n" + \
+"                      .dNMMMNy.        `./dMMMMh/`              \n" + \
+"                   `-/yMMMMmy.         .ohMMMdy:`               \n" + \
+"                  `:NNMMNdy:           `.:yNm+`                 \n" + \
+"                   .syMm+.                `/ms/`                \n" + \
+"                    -+No.                  `---`                \n" + \
+"                     .+:`                                       \n"
+
+frame5 = \
+"                                            `-::-.`             \n" + \
+"                                           .yNMMNdo             \n" + \
+"                                           .mMMMMms             \n" + \
+"                                          `-dMMMMms             \n" + \
+"                                      `--oydNMMNy+-             \n" + \
+"                                   `-omNMMMMMMMNo-  `           \n" + \
+"                                  :/hMMMMMMMMMMMmo.:o:.         \n" + \
+"                                `:NNMMMMMMMMMMMMMNdmNo-`        \n" + \
+"                                `-NMMNmNMMMMMMMMMMMMm/`         \n" + \
+"                                 `yhMMNMMMMMMMMMdmMM/.          \n" + \
+"                                  -/MMMMMMMMMMdy-.:/`           \n" + \
+"                                   `oNMMMMMMMMo:`               \n" + \
+"                                   `:NMMMMMMMMo-`               \n" + \
+"                                  `.hMMMMMMMMMmh+-              \n" + \
+"                                 `+oMMMMMMMMMMMMNs.             \n" + \
+"                                 `hdMMMMMMMMMMMMMNd/`           \n" + \
+"                                `-NNMMMMNNmNNMMMMMMmo-          \n" + \
+"                                -hMMMMNo:....+hmNMMMMmh:`       \n" + \
+"                               .oMMMMM+.      `./smMMMMy:       \n" + \
+"                           `.+hNMMMMMd`          .+MMMMy:       \n" + \
+"                       .:ohNNMMMMMMNd-           -sMMMMo.       \n" + \
+"                  ./yyydNMMMMMNmds+/.            :yMMMM+`       \n" + \
+"                  :yMMMMMNmys/.`                 :yMMMM/`       \n" + \
+"                  :yMNmyso/.`                    -yMMMN/`       \n" + \
+"                  /hyo.`                         -sMMMN/`       \n" + \
+"                  .:``                           .oMMMMh+-      \n" + \
+"                                                 `:hhhdmds-     \n" + \
+"                                                        ``      \n"
+
+frame6 = \
+"                                            ``..``              \n" + \
+"                                           `shmdhy`             \n" + \
+"                                           /mMMMMN.`            \n" + \
+"                                           /mMMMMM-`            \n" + \
+"                                        `-/hNMMMNd`             \n" + \
+"                                    `/shmMMMMMMh/               \n" + \
+"                                  `-smMMMMMMMMMms               \n" + \
+"                                 -sNMMMMMMMMMMMMm+`---`         \n" + \
+"                                 sNMMmNMMMMMMMMMMNymMd/         \n" + \
+"                                 /hMMmNMMMMMMMMMMMMNN/`         \n" + \
+"                                 `+NMMMMMMMMMMMmhmm+:`          \n" + \
+"                                  -yMMMMMMMMMmh/`..`            \n" + \
+"                                   `hNMMMMMMMy-`                \n" + \
+"                                   .mMMMMMMMMy:.                \n" + \
+"                                  -sMMMMMMMMMNms-               \n" + \
+"                                  /NMMMMMMMMMMMmo               \n" + \
+"                                  /NMMMMMMMMMMMNy`              \n" + \
+"                                 -sMMMMMdssNMMMMNd.             \n" + \
+"                                 +dMMMNd/  /dMMMNNh:.           \n" + \
+"                           ``-///hNMMMy/.   -sdNMMMdy.          \n" + \
+"                  `.-//oshmmNMMMMMMMMd/      ./yNMMNN:`         \n" + \
+"                  -ydNMMMMMMMMMNNNNNd:`        /dMMMMo.         \n" + \
+"                  -hmMmhhyyso/:-...`           `/NMMMd/         \n" + \
+"                  :ddh/-..``                    -hMMMNo.        \n" + \
+"                  -s+-                          `-NMMMmo`       \n" + \
+"                                                  +mMMMms+oo/.  \n" + \
+"                                                  `sdMMMMNmm/.  \n" + \
+"                                                   ``ohho-``    \n" + \
+"                                                      ``        \n"
+
+frame7 = \
+"                                            .                   \n" + \
+"                                          `-//:.`               \n" + \
+"                                        ``smMMMy/`              \n" + \
+"                                        `.yNMMMy+.              \n" + \
+"                                        ..yNMMMy+.              \n" + \
+"                                      .ohdNMMmo-`               \n" + \
+"                                   .:sNMMMMMMd.                 \n" + \
+"                                 ./sdMMMMMMMMM/`                \n" + \
+"                                -hNMMMMMMMMMMMs-                \n" + \
+"                                sNMMMMMMMMMMMMy:````            \n" + \
+"                                -hMMMMMMMMMMMMNmdddms`          \n" + \
+"                                 -omNMMMMMMMMNNmmddy:           \n" + \
+"                                  -sdMMMMMMMm+:-..`             \n" + \
+"                                  /mNMMMMMMNh`                  \n" + \
+"                                 `+NMMMMMMMNh`                  \n" + \
+"                                 -yMMMMMMMMMNo`                 \n" + \
+"                                 :yMMMMMMMMMNs`                 \n" + \
+"                                 .sMMMMMMMMMNo`                 \n" + \
+"                                 :yMMMMMMMMMMN:`                \n" + \
+"                     ```         /hMMMMNmmNMMMd:                \n" + \
+"                    /dmmdhdmmmmmmdNMMMMo:-dMMMMo-`              \n" + \
+"                   -yMMMMMMMMMMMMMMMMMm.` +dMMMhs.              \n" + \
+"                  .ymmhhhmmmmddddmmNmy-   .sNMMNm:              \n" + \
+"                  :/-`   ```     ````      :yMMMMo.             \n" + \
+"                  ``                       ./MMMMh/             \n" + \
+"                                            .hMMMMy-            \n" + \
+"                                             -mMMMmy            \n" + \
+"                                              ymMMMm+-.`        \n" + \
+"                                              `smNNddhs+.       \n" + \
+"                                               ```.```          \n"
+
+frames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7]
---
+++ terminal_animation.py
@@ -0,0 +1,30 @@
+
+""" terminal_animation (v1.0)
+
+A text-based animation which runs on the command line.
+The frames of the animation are loaded from the 'anim_frames' module.
+A frame is simply a string of characters and the animation consists of a list of frames.
+
+Author: mdq3
+2012/05/16
+
+"""
+
+import time
+import os
+import animation
+
+duration = 0.05    # The duration of time in seconds between each frame.
+cycles = 100       # The number of cycles the animation plays through.
+
+def animate():
+    """ A function that iterates through the frames, printing then clearing each one to create an animation. """
+    count = 0
+    while count < cycles:
+        for frame in animation.frames:
+            print '\033[1;34m' + frame + '\033[1;m'  # Print the frame in color blue.
+            time.sleep(duration)
+            print(os.system('clear'))
+        count = count + 1
+
+animate()
